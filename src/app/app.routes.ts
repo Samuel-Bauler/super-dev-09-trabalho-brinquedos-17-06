41c2c4c671334d31ccb8d30f2fb10717c96fc06a
Felipe <felipesaziki@gmail.com>
2026-06-17 20:47:07 -0300

    feito html de brinquedos-listar e brinquedos-listar.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { BrinquedoCadastrar } from './brinquedos/brinquendos-cadastrar/brinquedos-cadastrar';
+import { BrinquedosListar } from './brinquedos/brinquedos-listar/brinquedos-listar';
 
 
 export const routes: Routes = [
-    { path: "brinquedos/cadastrar", loadComponent: () => BrinquedoCadastrar}
+    { path: "brinquedos/cadastrar", loadComponent: () => BrinquedoCadastrar},
+    { path: "brinquedos/listar", loadComponent: () => BrinquedosListar}
 ];
--- src/app/brinquedos/brinquedos-listar/brinquedos-listar.ts
+++ src/app/brinquedos/brinquedos-listar/brinquedos-listar.ts
@@ -1,9 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
+import { BrinquedoModel } from '../../models/brinquedo.model';
 
 @Component({
   selector: 'app-brinquedos-listar',
-  imports: [],
+  imports: [CommonModule, RouterLink],
   templateUrl: './brinquedos-listar.html',
   styleUrl: './brinquedos-listar.scss',
 })
-export class BrinquedosListar {}
+export class BrinquedosListar {
+  brinquedos = signal<BrinquedoModel[]>([]);
+
+  ngOnInit(): void {
+    this.carregarBrinquedos();
+  }
+
+  carregarBrinquedos(): void {
+    const dadosSalvos = localStorage.getItem('Brinquedos');
+
+    if (dadosSalvos) {
+      this.brinquedos.set(JSON.parse(dadosSalvos));
+    } else {
+      this.brinquedos.set([]);
+    }
+  }
+}
